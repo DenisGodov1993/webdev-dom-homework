d4c0ac051082d85a8d0c987ab5092eb4b4eb96a2
DenisGodov1993 <denisgodovanyuk@gmail.com>
2025-06-17 16:32:25 +0300

    removed commented code

--- modules/utils.js
+++ modules/utils.js
@@ -1,5 +1,4 @@
 export function clearHTML(signs) {
-    // модуль только на экспорт, для других модулей
     return signs
         .replaceAll('&', '&amp;')
         .replaceAll('<', '&lt;')
@@ -7,13 +6,3 @@ export function clearHTML(signs) {
         .replaceAll('"', '&quot;')
         .replaceAll("'", '&apos;')
 }
-
-// function clearHTML(signs) {
-//     // заменяет в HTML небезопасные знаки на безопасные
-//     return signs
-//         .replaceAll('&', '&amp;')
-//         .replaceAll('<', '&lt;')
-//         .replaceAll('>', '&gt;')
-//         .replaceAll('"', '&quot;')
-//         .replaceAll("'", '&apos;')
-// }
